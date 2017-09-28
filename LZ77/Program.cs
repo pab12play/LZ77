5971368fea986cdfc47db974d693b085d10c935f
Pablo Pineda <pablo12.izq@gmail.com>
2017-09-27 21:04:55 -0600

    Fixed bug in table

--- LZ77/Program.cs
+++ LZ77/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LZ77
 {
@@ -10,28 +11,28 @@ namespace LZ77
     {
         static void Main(string[] args)
         {
-            string word = "abracadabra";
-            //string word = args[0];
+            string word = File.ReadAllText(args[0]);
             List<word> table = new List<LZ77.word>();
-            List<word> variable;
-            for(int currentLocation = 0; currentLocation < word.Length; currentLocation++)
+            List<word> variable = new List<LZ77.word>();
+            for (int currentLocation = 0; currentLocation < word.Length; currentLocation++)
             {
+                Console.Clear();
+                print(table);
                 //does it exist previously?
                 if (word.Substring(0,currentLocation).Contains(word[currentLocation]))
                 {
                     //if exist go and find it
-                    for (int searchBuffer = currentLocation-1; searchBuffer > 0; searchBuffer--)
+                    for (int searchBuffer = currentLocation-1; searchBuffer >= 0; searchBuffer--)
                     {
                         //when found it
                         if (word[currentLocation] == word[searchBuffer])
                         {
                             //how long it is the match?
-                            variable = new List<LZ77.word>();
-                            int lookAhead = 0;
+                            int lookAhead = currentLocation+1;
                             string temp = "" + word[searchBuffer];
-                            for(int searchIndex = searchBuffer; searchIndex < currentLocation; searchIndex++)
+                            for(int searchIndex = searchBuffer+1; searchIndex < currentLocation; searchIndex++)
                             {
-                                if (currentLocation+lookAhead < word.Length)
+                                if (lookAhead < word.Length)
                                 {
                                     //is the lookAhead equal to the next character in buffer
                                     if (word[searchIndex] == word[lookAhead])
@@ -41,27 +42,32 @@ namespace LZ77
                                     }
                                     else
                                     {
-                                        variable.Add(new LZ77.word(temp,searchBuffer,temp.Length));
+                                        variable.Add(new LZ77.word(temp,currentLocation - searchBuffer,temp.Length));
                                         break;
                                     }
                                 }
                                 else
                                 {
-                                    variable.Add(new LZ77.word(temp, searchBuffer, temp.Length));
+                                    variable.Add(new LZ77.word(temp, currentLocation - searchBuffer, temp.Length));
                                     break;
                                 }
                             }
-                            //order and find the longest match
-                            word longest = variable.OrderByDescending(x => x.Length).First();
-                            if (currentLocation + longest.Length < word.Length)
-                            {
-                                table.Add(new word(""+word[currentLocation+longest.Length], longest.Offset, longest.Length));
-                            }
-                            else
-                            {
-                                table.Add(new word("", longest.Offset, longest.Length ));
-                            }
                         }
+                    }
+                    //order and find the longest match
+                    if (variable.Count() > 0)
+                    {
+                        word longest = variable.OrderByDescending(x => x.Length).First();
+                        variable.Clear();
+                        if (currentLocation + longest.Length < word.Length)
+                        {
+                            table.Add(new word("" + word[currentLocation + longest.Length], longest.Offset, longest.Length));
+                        }
+                        else
+                        {
+                            table.Add(new word("", longest.Offset, longest.Length));
+                        }
+                        currentLocation = currentLocation + longest.Length;
                     }
                 }
                 else
@@ -69,12 +75,19 @@ namespace LZ77
                     table.Add(new word(""+word[currentLocation],0,0));
                 }
             }
+            Console.Clear();
+            print(table);
+            Console.ReadLine();
+            
+        }
+
+        static void print(List<word> table)
+        {
             Console.WriteLine("Offset\tLength\tSymbol");
             foreach (word w in table)
             {
                 Console.WriteLine(w.Offset + "\t" + w.Length + "\t" + w.Name);
             }
-            Console.ReadLine();
         }
 
     }
